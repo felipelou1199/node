9a4c5c47655523d89035710d678ab3041d7e695d
Felipe Rosas <felipe.rosas1199@gmail.com>
2025-04-27 21:31:18 -0600

    Creation of pages using EJS, and using templates that i used in different parts for the html views

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const express = require('express');
 // express app 
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 // listen for request 
 app.listen(3000);
 
 app.get('/', (req,res) =>{
-
-    // res.send('<p>inicio</p>');
-    res.sendFile('./views/index.html',{root: __dirname});
+    const blogs = [
+        {tittle: 'Min el amor de mi vida', snippet: 'Como me mega encanta mi novia min toda hermosa y preciosa'},
+        {tittle: 'Porque amo a min', snippet: 'Min es la mujer mas increble que existe en todo el mundo, nadamas es la top 1'},
+        {tittle: 'Min casate conmigo', snippet: 'Min ya te di el anillo ni modo que no nos casemos mañana'}
+    ];
+    res.render('index',{tittle: 'Home',blogs});
 });
 app.get('/about', (req,res) =>{
-    // res.send('<p>about</p>');
-    res.sendFile('./views/about.html',{root: __dirname});
+    res.render('about',{tittle: 'About'})
 });
-//redirects
-app.get('/about-us', (req,res) =>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res) =>{
+    res.render('create',{tittle: 'Create a new Blog'});
 });
-
 //error 404
 app.use((req,res) =>{
-    res.status(404).sendFile('./views/404.html',{root: __dirname});
+    res.status(404).render('404',{tittle: '404'});
 });
